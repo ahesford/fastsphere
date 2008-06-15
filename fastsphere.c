522383eb3bc69ec0b4570ec72c67f83be1de2414
jester <devnull@localhost>
2008-06-15 23:06:33 +0000

    Handle excitation of multiple spheres.

--- fastsphere.c
+++ fastsphere.c
@@ -1,33 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <complex.h>
+#include <math.h>
+
 #include "fastsphere.h"
 #include "config.h"
 #include "init.h"
 #include "scatmat.h"
 
 int main (int argc, char **argv) {
-	int nspheres, nsptype, n, i;
+	int nspheres, nsptype, n, i, j, nterms;
 	sptype *sparms;
 	spscat *slist;
 	bgtype bg;
 	exctparm exct;
 	shdata shtr;
 	itconf itc;
-	complex double **trans, *rhs, *sol;
+	complex double **trans, *rhs, *sol, val;
+	double sdir[3], phase;
 
 	readcfg (stdin, &nspheres, &nsptype, &sparms, &slist, &bg, &exct);
 	sphinit (sparms, nsptype, &bg, &shtr);
 	sphbldfmm (&trans, slist, nspheres, &bg, &shtr);
 
-	itc.iter = 100; itc.restart = 10; itc.eps = 1e-3;
+	itc.iter = 100; itc.restart = 10; itc.eps = 1e-6;
 
-	n = nspheres * shtr.ntheta * shtr.nphi;
+	nterms = shtr.ntheta * shtr.nphi;
+	n = nspheres * nterms;
 	rhs = calloc (2 * n, sizeof(complex double));
 	sol = rhs + n;
+	
+	sdir[0] = cos(exct.phi) * sin(exct.theta);
+	sdir[1] = sin(exct.phi) * sin(exct.theta);
+	sdir[2] = cos(exct.theta);
 
-	for (i = 0; i < shtr.nphi; ++i)
-		rhs[i] = 1.0 / (double)shtr.nphi;
+	/* Set the excitation for each sphere. */
+	for (j = 0; j < nspheres; ++j) {
+		phase = sdir[0] * slist[j].cen[0] + sdir[1] * slist[j].cen[1]
+			+ sdir[2] * slist[j].cen[2];
+		val = cexp (-I * phase) / (double)shtr.nphi;
+		for (i = 0; i < shtr.nphi; ++i)
+			rhs[i] = val;
+	}
 
 	buildrhs (rhs, slist, nspheres, &shtr);
 
