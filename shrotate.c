78dc624c2ba399ea848489ffba523b95dc68cc62
jester <devnull@localhost>
2008-06-23 14:29:54 +0000

    The phi angle is always pi/2, so don't bother computing it.

--- shrotate.c
+++ shrotate.c
@@ -3,10 +3,11 @@
 #include <math.h>
 #include <float.h>
 
-int getangles (double *theta, double *phi, double *chi, double axis[3]) {
+/* Find the polar and rotation angles of the new z-axis for translation. */
+int getangles (double *theta, double *chi, double axis[3]) {
 	double st;
 
-	*theta = acos (axis[2]); /* The polar rotation angle. */
+	*theta = acos (axis[2]); /* The polar angle of the new z-axis. */
 
 	st = sqrt (axis[0] * axis[0] + axis[1] * axis[1]);
 
@@ -16,12 +17,10 @@ int getangles (double *theta, double *phi, double *chi, double axis[3]) {
 		return 0;
 	}
 
-	/* The old z-axis is always at phi = pi / 2. This is thre result of an
-	 * arbitrary choice of the new x- and y-axis orientations. */
-	*phi = M_PI_2;
-
-	/* The rotation angle of the new z-axis must derive from the y-axis
-	 * coordinate, since the x-axis coordinate uses the even cosine. */
+	/* The y-coordinate of the new z-axis vector is given by
+	 * y = sin(theta) sin(chi). Hence, chi is easily computed. Note
+	 * that the x-coordiante is x = sin(theta) cos(chi), but this
+	 * doesn't work well, since cosine is an even function. */
 	*chi = asin (axis[1] / st);
 
 	return 1;
